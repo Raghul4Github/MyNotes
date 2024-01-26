498d7f40066e0e794e2672ef81902990db0296b7
Raghul4Github <157718141+Raghul4Github@users.noreply.github.com>
2024-01-26 07:12:55 +0530

    Add files via upload

---
+++ assets/books.ts
@@ -0,0 +1,29 @@
+export interface books
+{
+//  "UserId":number;
+//  "id":number;
+ book_id:number,
+ book_name:String,
+ aurthor:String,
+ genere:String,
+year:String
+
+}
+// "bookId":1,
+//       "bookTitle":"law of attraction",
+//       "aurthor":"rhonda ",
+
+// b1_0.book_id,
+// b1_0.aurthor,
+// b1_0.book_name,
+// b1_0.genere,
+// b1_0.year 
+//       "genere":"mind",
+//       "year":1999
+
+
+// private int bookId;
+// 	private String bookName;
+// 	private String aurthor;
+// 	private String genere;
+// 	private String year;
